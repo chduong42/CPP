75b1d46eba1f751aafa24c7b89b76683b37f06f7
Chi-khang DUONG <chduong@e1r5p8.42.fr>
2022-07-05 13:48:17 +0200

    gitingore

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chduong <chduong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 17:20:16 by chduong           #+#    #+#             */
+/*   Updated: 2022/06/21 11:42:14 by chduong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (size_t j = 0; j < strlen(av[i]); j++)
+				av[i][j] = std::toupper(av[i][j]);
+			std::cout << av[i];
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
