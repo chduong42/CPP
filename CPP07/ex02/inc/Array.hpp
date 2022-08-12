2bdb28da65301c1c3c28c438ed284050257d7b8b
Chi-khang DUONG <chduong@e1r4p2.clusters.42paris.fr>
2022-08-12 18:33:56 +0200

    finish cpp07

--- CPP07/ex02/inc/Array.hpp
+++ CPP07/ex02/inc/Array.hpp
@@ -6,23 +6,67 @@
 /*   By: chduong <chduong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 19:56:36 by chduong           #+#    #+#             */
-/*   Updated: 2022/08/11 19:59:41 by chduong          ###   ########.fr       */
+/*   Updated: 2022/08/12 17:50:56 by chduong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
-# define ARRAY__HPP
+# define ARRAY_HPP
+# include <exception>
+# include <iostream>
+
+typedef unsigned int uint;
 
 template<typename T>
 class Array {
-    public:
-        Array();
-        Array(unsigned int n);
-        Array(Array const &src);
-        ~Array();
+	private:
+		T*			_array;
+		uint		_n;
 
-        Array&      operator=(Array const &src);
-    
+	public:
+		Array() : _array(NULL), _n(0) {}
+		Array(uint const n) : _array(new T[n]), _n(n) {}
+		Array(Array<T> const &src) : _array(NULL), _n(0) { *this = src;}
+		~Array() {
+			if (this->_n > 0)
+				delete [] this->_array;
+		}
+
+		Array&  operator=(Array<T> const &rhs)
+		{
+			if (this != &rhs)
+			{
+				if (this->_array)
+					delete [] this->_array;
+				this->_n = rhs._n;
+				this->_n ? this->_array = new T [this->_n] : this->_array = NULL;
+				for (uint i = 0; i < rhs._n; i++)
+					this->_array[i] = rhs._array[i];
+			}
+			return *this;
+		}
+
+		T&		operator[](uint index) {
+			if (index >= this->_n)
+				throw Array::OutOfLimitsException();
+			return this->_array[index];
+		}
+
+		uint		size(void) const {
+			return this->_n;
+		}
+
+		void		printOut(void) const {
+			for (uint i = 0; i < this->_n; i++)
+				std::cout << "[" << i << "]: " << this->_array[i] << std::endl;
+		}
+
+		class OutOfLimitsException : public std::exception {
+			public:
+				virtual void printWhat() const throw() {
+					std::cerr << "Access denied : you are out of limits" << std::endl;
+				}
+		};
 };
 
 #endif
--- CPP07/ex02/src/main.cpp
+++ CPP07/ex02/src/main.cpp
@@ -1,53 +1,89 @@
-#include <iostream>
-#include <Array.hpp>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chduong <chduong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/12 16:23:57 by chduong           #+#    #+#             */
+/*   Updated: 2022/08/12 18:32:43 by chduong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define MAX_VAL 750
+#include <Array.hpp>
+#include <cstdlib>
+
+#define MAX_VAL 10
+
 int main(int, char**)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand() % 101;
+		numbers[i] = value;
+	}
+	std::cout << "\n================ numbers content ================" << std::endl;
+	numbers.printOut();
+		
+	std::cout << "\n================ Test of overload assignment (tmp = numbers) ================" << std::endl;
+	Array<int> tmp = numbers;
+	tmp.printOut();
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+	std::cout << "\n================ Test Copy Construtor (test(numbers)) ================" << std::endl;
+	Array<int> test(numbers);
+	test.printOut();
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+
+	std::cout << "\n================ numbers content cleaning ================" << std::endl;
+	for (int i = 0; i < MAX_VAL; i++)
+		numbers[i] = 0;
+	numbers.printOut();
+
+	std::cout << "\n================ Check of tmp ================" << std::endl;
+	tmp.printOut();
+	std::cout << "\n================ Check of test ================" << std::endl;
+	test.printOut();
+
+	std::cout << "\n================ Test negative index ================" << std::endl;
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch(const Array<int>::OutOfLimitsException& e)
+	{
+		e.printWhat();
+	}
+
+	std::cout << "\n================ Test out index ================" << std::endl;
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const Array<int>::OutOfLimitsException& e)
+	{
+		e.printWhat();
+	}
+
+	std::cout << "\n================ Test of leaks ================" << std::endl;
+	std::cout << "\n---------------- init tmp2 with size 5 and value 1 ----------------" << std::endl;
+	Array<int> tmp2(5);
+	for (int i = 0; i < 5; i++)
+		tmp2[i] = 1;
+	tmp2.printOut();
+	std::cout << "\n---------------- tmp2 = numbers ----------------" << std::endl;
+	tmp2 = numbers;
+	tmp2.printOut();
+	
+	return 0;
 }
