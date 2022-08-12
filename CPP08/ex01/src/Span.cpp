3054eac45493638f741a48f7fa4f7b78c45be535
Chi-khang DUONG <chduong@e1r4p2.clusters.42paris.fr>
2022-08-12 21:55:16 +0200

    corr

--- CPP08/ex01/src/Span.cpp
+++ CPP08/ex01/src/Span.cpp
@@ -6,21 +6,21 @@
 /*   By: chduong <chduong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 21:07:11 by chduong           #+#    #+#             */
-/*   Updated: 2022/08/12 21:17:25 by chduong          ###   ########.fr       */
+/*   Updated: 2022/08/12 21:52:55 by chduong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-Span::Span(): _tab(NULL), _N(0) {
+Span::Span(): _V(NULL), _N(0) {
 	return;
 }
 
-Span::Span(uint N): _tab(new int[N]), _N(N) {
+Span::Span(uint N): _V(new int[N]), _N(N) {
 	return;
 }
 
-Span::Span(Span const &src) : _tab(NULL), _N(0) {
+Span::Span(Span const &src) : _V(NULL), _N(0) {
 	*this = src;
 }
 
@@ -28,9 +28,7 @@ Span&	Span::operator=(Span const &src)
 {
 	if (this != &src)
 	{
-		if (this->_tab)
-			delete [] this->_tab;
-		this->_tab = new int[src._N];
+		this->_V = src._V;
 		this->_N = src._N;	
 	}
 	return *this;
